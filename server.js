467fd9f0bd80f21efcc31c6aebfa9b227e88ddf6
Kennith Nichol <1321022+kennithnichol@users.noreply.github.com>
2018-10-09 13:15:32 -0700

    implement postgres database with knex

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
-const db = ({
+const db = knex({
 	client: 'pg',
 	connection: {
 		host: '127.0.0.1',
@@ -16,106 +16,85 @@ const db = ({
 
 const server = express();
 
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john.smith@example.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'Sally',
-			email: 'sally.smith@example.com',
-			password: 'bananas',
-			entries: 0,
-			joined: new Date()
-		}
-	],
-	login: [
-		{
-			id: 987,
-			hash: '',
-			email: 'john.smith@example.com'
-		},
-		{
-			id: 988,
-			hash: '',
-			email: 'sally.smith@example.com'
-		}
-	]
-}
-
 server.use(bodyParser.json());
 server.use(cors());
 
 server.get('/', (req, res) => {
-	res.json(database.users);
+	db.select('*').from('users')
+		.then(users => {
+			res.json(users)
+		})
+		.catch(err => {
+			res.status(404).json('error getting users')
+		});
 })
 
 server.post('/signin', (req, res) => {
 	const { email, password } = req.body;
-
-	if (database.users[0].email === email &&
-		database.users[0].password === password) {
-		res.json(database.users[0]);
-	} else {
-		res.status(400).json('error logging in');
-	}
+	db.select('email','hash').from('login')
+		.where('email', '=', email)
+		.then(login => {
+			return bcrypt.compare(password, login[0].hash, (err, valid) => {
+				if (valid) {
+					return db.select('*').from('users')
+						.where('email', '=', email)
+						.then(user => res.json(user[0]))
+						.catch(err => res.status(400).json('unable to get user'))
+				}
+				res.status(400).json('invalid credentials');
+			})
+		})
+		.catch(err => res.status(400).json('invalid credentials'))
 })
 
 server.post('/register', (req, res) => {
 	const{ email, name, password } = req.body;
 	bcrypt.hash(password, null, null, (err, hash) => {
-		database.login.push({
-			id: database.login.length,
-			email: email,
-			hash: hash
-		});
-	});
-	database.users.push({
-		id: database.users.length + 123,
-		email: email,
-		name: name,
-		entries: 0,
-		joined: new Date()
-	});
-	res.json(database.users[database.users.length-1]);
+		db.transaction(trx => {
+			trx.insert({
+				hash: hash,
+				email: email
+			})
+			.into('login')
+			.returning('email')
+			.then(loginEmail => {
+				return trx('users')
+					.returning('*')
+					.insert({
+						name: name,
+						email: loginEmail[0],
+						joined: new Date()
+						})
+					})
+					.then(user => {
+						res.json(user[0]);
+					})
+			.then(trx.commit)
+			.catch(trx.rollback)
+		})
+		.catch(err => res.status(400).json('unable to register'))
+	})
 })
 
 server.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		}
-	})
-
-	if (!found) {
-		res.status(400).json('no such user');
-	}
+	db.select('*').from('users').where({id})
+		.then(user => {
+			if (user.length) {
+				res.json(user[0])
+			} else {
+				res.status(400).json('user not found')
+			}
+		})
+		.catch(err => res.status(400).json('error getting user'))
 })
 
 server.put('/image', (req, res) => {
 	const { id } = req.body;
-	let found = false;
-
-	database.users.forEach(user => {
-		if (user.id == id) {
-			found = true;
-			user.entries++
-			return res.json(user.entries);
-		}
-	})
-
-	if (!found) {
-		res.status(400).json('no such user');
-	}
+	db('users').where({id}).increment('entries', 1)
+		.returning('entries')
+		.then(entries => res.json(entries[0]))
+		.catch(err => res.status(400).json('error processing request'));
 })
 server.listen(3000, () => {
 	console.log('Server is running on port 3000');
