0d4aff1822ea21884dcf161a57b59b33512f8189
Kennith Nichol <1321022+kennithnichol@users.noreply.github.com>
2018-10-25 15:51:30 -0700

    add authentication to signin

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,22 +1,36 @@
-const handleSignin = (db, bcrypt) =>  (req, res) => {
+const handleSignin = (db, bcrypt, req, res) => {
 	const { email, password } = req.body;
 	if (!email || !password) {
-		return res.status(400).json('invalid form submission');
+		return Promise.reject('invalid form submission');
 	}
-	db.select('email','hash').from('login')
+
+	return db.select('email','hash')
+		.from('login')
 		.where('email', '=', email)
-		.then(login => {
-			return bcrypt.compare(password, login[0].hash, (err, valid) => {
-				if (valid) {
-					return db.select('*').from('users')
-						.where('email', '=', email)
-						.then(user => res.json(user[0]))
-						.catch(err => res.status(400).json('unable to get user'))
-				}
-				res.status(400).json('invalid credentials');
-			})
+		.then(login => bcrypt.compare(password, login[0].hash))
+		.then(res => {
+			if (!res) return Promise.reject();
+
+			return db.select('*').from('users')
+				.where('email', '=', email)
+				.then(user => user[0])
+				.catch(err => Promise.reject())
 		})
-		.catch(err => res.status(400).json('invalid credentials'))
+		.then( data => data )
+		.catch(err => Promise.reject(`invalid credntials`))
 }
 
-module.exports = { handleSignin }
+const getAuthToken = () => {
+	console.log('auth ok');
+}
+
+const authSignin = (db, bcrypt) => (req, res) => {
+	const { authorization } = req.headers;
+	return authorization ?
+		getAuthToken() :
+		handleSignin(db, bcrypt, req, res)
+			.then(data => res.json(data))
+			.catch(err => res.status(400).json(err) );
+}
+
+module.exports = { authSignin }
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
 const morgan = require('morgan');
@@ -26,7 +26,7 @@ server.use(cors());
 server.use(morgan('combined'));
 
 server.get('/', index.handleIndex(db))
-server.post('/signin', signin.handleSignin(db, bcrypt))
+server.post('/signin', signin.authSignin(db, bcrypt))
 server.post('/register', register.handleRegister(db, bcrypt))
 server.get('/profile/:id', profile.handleProfileGet(db))
 server.post('/profile/:id', profile.handleProfileUpdate(db))
